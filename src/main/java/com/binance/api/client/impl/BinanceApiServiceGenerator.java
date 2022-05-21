e6844e1930eef2b1e17c9f0ac0d63d61dc658647
Dimas Victor <quetzalcroix@gmail.com>
2022-05-22 02:42:55 +0700

    Added proxy settings for

--- src/main/java/com/binance/api/client/impl/BinanceApiServiceGenerator.java
+++ src/main/java/com/binance/api/client/impl/BinanceApiServiceGenerator.java
@@ -1,20 +1,24 @@
 package com.binance.api.client.impl;
 
+import java.io.IOException;
+import java.net.Authenticator;
 import java.net.InetSocketAddress;
+import java.net.PasswordAuthentication;
 import java.net.Proxy;
 import java.util.concurrent.TimeUnit;
 
+import okhttp3.*;
 import org.apache.commons.lang3.StringUtils;
 
 import com.binance.api.client.BinanceApiWebSocketClient;
 import com.binance.api.client.config.BinanceApiConfig;
 import com.binance.api.client.security.AuthenticationInterceptor;
 
-import okhttp3.Dispatcher;
-import okhttp3.OkHttpClient;
 import retrofit2.Converter;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
+
+import javax.annotation.Nullable;
 
 /**
  * Generates a Binance API implementation based on @see
@@ -22,47 +26,53 @@ import retrofit2.converter.jackson.JacksonConverterFactory;
  */
 public class BinanceApiServiceGenerator implements ApiGenerator {
 
-  private final OkHttpClient sharedClient;
-  private final Converter.Factory converterFactory = JacksonConverterFactory.create();
+    private final OkHttpClient sharedClient;
+    private final Converter.Factory converterFactory = JacksonConverterFactory.create();
 
-  private final String hostname = "159.65.4.199";
-  private final int port = 3128;
-  private final Proxy proxy = new Proxy(Proxy.Type.SOCKS, new InetSocketAddress(hostname, port));
+    private final String hostname = "159.65.4.199";
+    private final int port = 3128;
+    private final Proxy proxy = new Proxy(Proxy.Type.SOCKS, new InetSocketAddress(hostname, port));
 
-  {
-    Dispatcher dispatcher = new Dispatcher();
-    dispatcher.setMaxRequestsPerHost(500);
-    dispatcher.setMaxRequests(500);
-    sharedClient = new OkHttpClient.Builder()
-            .dispatcher(dispatcher)
-            .proxy(proxy)
-            .pingInterval(20, TimeUnit.SECONDS)
-            .build();
-  }
-
-  @Override
-  public <S> S createService(Class<S> serviceClass, String apiKey, String secret) {
-    Retrofit.Builder retrofitBuilder = new Retrofit.Builder().baseUrl(BinanceApiConfig.getApiBaseUrl())
-        .addConverterFactory(converterFactory);
-
-    if (StringUtils.isEmpty(apiKey) || StringUtils.isEmpty(secret)) {
-      retrofitBuilder.client(sharedClient);
-    } else {
-      // `adaptedClient` will use its own interceptor, but share thread pool etc with
-      // the 'parent' client
-      AuthenticationInterceptor interceptor = new AuthenticationInterceptor(apiKey, secret);
-      OkHttpClient adaptedClient = sharedClient.newBuilder().addInterceptor(interceptor).build();
-      retrofitBuilder.client(adaptedClient);
+    {
+        Dispatcher dispatcher = new Dispatcher();
+        dispatcher.setMaxRequestsPerHost(500);
+        dispatcher.setMaxRequests(500);
+        sharedClient = new OkHttpClient.Builder()
+                .dispatcher(dispatcher)
+                .proxy(proxy)
+                .proxyAuthenticator((route, response) -> {
+                    String credential = Credentials.basic("vextabit", "vgm2022");
+                    return response.request().newBuilder()
+                            .header("Proxy-Authorization", credential)
+                            .build();
+                })
+                .pingInterval(20, TimeUnit.SECONDS)
+                .build();
     }
 
-    Retrofit retrofit = retrofitBuilder.build();
-    return retrofit.create(serviceClass);
-  }
+    @Override
+    public <S> S createService(Class<S> serviceClass, String apiKey, String secret) {
+        Retrofit.Builder retrofitBuilder = new Retrofit.Builder().baseUrl(BinanceApiConfig.getApiBaseUrl())
+                .addConverterFactory(converterFactory);
 
-  @Override
-  public BinanceApiWebSocketClient createSocket() {
-    return new BinanceApiWebSocketClientImpl(sharedClient);
+        if (StringUtils.isEmpty(apiKey) || StringUtils.isEmpty(secret)) {
+            retrofitBuilder.client(sharedClient);
+        } else {
+            // `adaptedClient` will use its own interceptor, but share thread pool etc with
+            // the 'parent' client
+            AuthenticationInterceptor interceptor = new AuthenticationInterceptor(apiKey, secret);
+            OkHttpClient adaptedClient = sharedClient.newBuilder().addInterceptor(interceptor).build();
+            retrofitBuilder.client(adaptedClient);
+        }
 
-  }
+        Retrofit retrofit = retrofitBuilder.build();
+        return retrofit.create(serviceClass);
+    }
+
+    @Override
+    public BinanceApiWebSocketClient createSocket() {
+        return new BinanceApiWebSocketClientImpl(sharedClient);
+
+    }
 
 }
